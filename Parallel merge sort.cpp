8d8979543ab969e8cc7a4237fce92fb67ed44538
shubman <118682078+embaku@users.noreply.github.com>
2023-05-30 16:16:49 +0530

    Update Parallel merge sort.cpp
    Updated code

--- Parallel merge sort.cpp
+++ Parallel merge sort.cpp
@@ -1,51 +1,77 @@
 #include <iostream>
 #include <omp.h>
+
 using namespace std;
-void merge(int arr[], int left, int mid, int right) {
- int n1 = mid - left + 1;
- int n2 = right - mid;
- int L[n1], R[n2];
- for (int i = 0; i < n1; i++) {
- L[i] = arr[left + i];
- }
- for (int i = 0; i < n2; i++) {
- R[i] = arr[mid + 1 + i];
- }
- int i = 0, j = 0, k = left;
- while (i < n1 && j < n2) {
- if (L[i] <= R[j]) {
- arr[k++] = L[i++];
- } else {
- arr[k++] = R[j++];
- }
- }
- while (i < n1) {
- arr[k++] = L[i++];
- }
- while (j < n2) {
- arr[k++] = R[j++];
- }
+
+void merge(int arr[], int left[], int leftSize, int right[], int rightSize) {
+    int i = 0, j = 0, k = 0;
+    while (i < leftSize && j < rightSize) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+        }
+    }
+    while (i < leftSize) {
+        arr[k++] = left[i++];
+    }
+    while (j < rightSize) {
+        arr[k++] = right[j++];
+    }
 }
-void parallelMergeSort(int arr[], int left, int right) {
- if (left < right) {
- int mid = left + (right - left) / 2;
- #pragma omp parallel sections
- {
- #pragma omp section
- parallelMergeSort(arr, left, mid);
- #pragma omp section
- parallelMergeSort(arr, mid + 1, right);
- }
- merge(arr, left, mid, right);
- }
+
+void mergeSort(int arr[], int size) {
+    if (size <= 1) {
+        return;
+    }
+
+    int mid = size / 2;
+    int left[mid];
+    int right[size - mid];
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            // Copying elements to the left half
+            for (int i = 0; i < mid; i++) {
+                left[i] = arr[i];
+            }
+            // Sorting the left half in parallel
+            mergeSort(left, mid);
+        }
+
+        #pragma omp section
+        {
+            // Copying elements to the right half
+            for (int i = mid; i < size; i++) {
+                right[i - mid] = arr[i];
+            }
+            // Sorting the right half in parallel
+            mergeSort(right, size - mid);
+        }
+    }
+
+    merge(arr, left, mid, right, size - mid);
 }
+
 int main() {
- int arr[] = {8, 1, 2, 7, 4, 5};
- int n = sizeof(arr) / sizeof(arr[0]);
- parallelMergeSort(arr, 0, n - 1);
- for (int i = 0; i < n; i++) {
- cout << arr[i] << " ";
- }
- cout << endl;
- return 0;
+    int size;
+    cout << "Enter length of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter array elements: " << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
